a6c27347e5973c6a4b56008c246c662329e70642
edmund27 <epafko@gmail.com>
2020-06-19 12:01:19 +0200

    Created a route and base for the chat-screen

---
+++ src/pages/ChatScreen/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function ChatScreen() {
+
+    return (
+        <div>
+            <h4>MessengeR</h4>
+            <p>here go messages</p>
+            <p>here you type a message</p>
+        </div>
+    );
+}
